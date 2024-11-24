8a98904598dbcd6ff1b73a8798ff5d32b9070621
Léo Guirado <leo.guirado@routard.com>
2024-11-24 14:58:36 +0100

    feat(node): update mongoose & product

--- node/src/index.js
+++ node/src/index.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const productRoutes = require('./routes/productRoutes');
-const connectionDB = require('./services/mongooseService');
+const express = require("express");
+const dotenv = require("dotenv");
+const productRoutes = require("./routes/productRoutes");
+const connectionDB = require("./services/mongooseService");
 
 const app = express();
 dotenv.config();
 
 app.use(express.json());
 
-app.use('/product', productRoutes);
+app.use("/product", productRoutes);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-    
-    connectionDB();
+  console.log(`Server is running on http://localhost:${port}`);
+
+  connectionDB();
 });
--- node/src/services/mongooseService.js
+++ node/src/services/mongooseService.js
@@ -1,19 +1,20 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const mongoose = require('mongoose');
-
-const mongo_uri = process.env.MONGODB_URI;
-
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-
-    await mongoose.connect(mongo_uri)
-        .then(() => {
-            console.log('Connected to MongoDB');
-        })
-        .catch((error) => {
-            console.error(error);
-        });
+  try {
+    mongoose.connect(process.env.MONGODB_URI, {
+      auth: {
+        username: process.env.MONGODB_USERNAME,
+        password: process.env.MONGODB_PASSWORD,
+      },
+      authSource: "admin",
+      dbName: process.env.DATABASE_NAME,
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 module.exports = connectDB;
